4fa29e14a7d133cd6dec25f2a38c1a4b43e8477a
suniel11 <sunilsingh00can@gmail.com>
2024-12-15 13:45:21 -0700

    Setting up the routes for components and  pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes , Route} from 'react-router-dom';
+import Home from './pages/Homepage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Siderbar from './components/sidebar';
+import Homepage from './pages/Homepage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
+  
+<div className="flex flex-col items-center justify-center h-screen bg" >
+<Header />
+<h1 className='text-2xl font-bold underline '> Heading</h1>
+
+
+<Routes>
+  <Route path="/" element={<Homepage/>} />
+  <Route path="/login" element={<Login/>} />
+  <Route path="/register" element={<Register/>} />
+
+
+    
+</Routes>
+
+</div> 
   )
 }
 
